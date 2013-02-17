1fda0a67b5d21c5de5d810f5e89ea2a66f842b04
flashtools <flashtools@gmail.com>
2013-02-17 04:37:19 +0000

    使用最新测试版Haxe

--- miniwebgl/src/MinimalDraw.hx
+++ miniwebgl/src/MinimalDraw.hx
@@ -1,7 +1,12 @@
 package;
-import UserAgentContext;
+import js.Boot;
+import js.Browser;
+import js.html.CanvasElement;
+import js.html.Event;
+import js.html.Float32Array;
+import js.html.webgl.Program;
+import js.html.webgl.RenderingContext;
 import js.Lib;
-import js.Dom;
 import WebGLUtil;
 /**
  * ...
@@ -14,38 +19,38 @@ class MinimalDraw
 		new MinimalDraw();
 	}
 	
-	private var gl:WebGLRenderingContext;
+	private var gl:RenderingContext;
 	public function new()
 	{
 		this.gl = null;
-		Lib.window.onload = onLoad;
+		Browser.window.onload = onLoad;
 	}
 	
-	private function onLoad(e:js.Event):Void
+	private function onLoad(e:Event):Void
 	{
-		var canvas:HTMLCanvasElement = cast Lib.document.getElementById("webgl_canvas");
-		gl = cast(canvas.getContext("experimental-webgl"),WebGLRenderingContext);
+		var canvas:CanvasElement = cast Browser.document.getElementById("webgl_canvas");
+		gl = cast(canvas.getContext("experimental-webgl"),RenderingContext);
 		gl.viewport(0, 0, canvas.width, canvas.height);
 		gl.clearColor(0, 0, 0.8, 1);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		gl.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
 		
 		var vertexPosBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
+		gl.bindBuffer(GL.ARRAY_BUFFER, vertexPosBuffer);
 		
 		var vertices:Array<Float> = [ -0.5, -0.5, 0.5, -0.5, 0, 0.5];
 		var floatArray:Float32Array = new Float32Array(vertices);
-		gl.bufferData(gl.ARRAY_BUFFER, floatArray, gl.STATIC_DRAW);
+		gl.bufferData(GL.ARRAY_BUFFER, floatArray, GL.STATIC_DRAW);
 		
 		var vs:String = "attribute vec2 pos;\n" +
 						"void main(){ gl_Position = vec4(pos,0,1); }";
 		var fs:String = "precision mediump float;\n" +
 						"void main() { gl_FragColor = vec4(0,0.8,0,1); }";
 		
-		var program:WebGLProgram = WebGLUtil.createProgram(gl,vs, fs);
+		var program:Program = WebGLUtil.createProgram(gl,vs, fs);
 		gl.useProgram(program);
 		var index:Int = gl.getAttribLocation(program, "pos");
 		gl.enableVertexAttribArray(index);
-		gl.vertexAttribPointer(index, 2, gl.FLOAT, false, 0, 0);
-		gl.drawArrays(gl.TRIANGLES, 0, 3);
+		gl.vertexAttribPointer(index, 2, GL.FLOAT, false, 0, 0);
+		gl.drawArrays(GL.TRIANGLES, 0, 3);
 	}
 }
